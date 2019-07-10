f155c19572f41e52c4d001504da6e72dd10dfaa6
Garald <lordgeck@gmail.com>
2019-07-10 23:44:44 +0200

    contact page and mailer

---
+++ src/Entity/Contact.php
@@ -0,0 +1,196 @@
+<?php
+
+namespace App\Entity;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+class Contact {
+
+    /**
+     * @var string|null
+     * @Assert\NotBlank()
+     * @Assert\Length(min=2, max=100)
+     */
+    private $firstName;
+
+    /**
+     * @var string|null
+     * @Assert\NotBlank()
+     * @Assert\Length(min=2, max=100)
+     */
+    private $lastName;
+
+    /**
+     * @var string|null
+     * @Assert\NotBlank()
+     * @Assert\Regex(
+     *   pattern="/[0-9]{10}/"
+     * )
+     */
+    private $phone;
+
+    /**
+     * @var string|null
+     * @Assert\NotBlank()
+     * @Assert\Email()
+     */
+    private $email;
+
+    /**
+     * @var string|null
+     * @Assert\NotBlank()
+     * @Assert\Length(min=10)
+     */
+    private $message;
+
+    /**
+     * @var Pokemon|null
+     */
+    private $pokemon;
+
+    
+
+    /**
+     * Get the value of firstName
+     *
+     * @return  string|null
+     */ 
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * Set the value of firstName
+     *
+     * @param  string|null  $firstName
+     *
+     * @return  self
+     */ 
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of lastName
+     *
+     * @return  string|null
+     */ 
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * Set the value of lastName
+     *
+     * @param  string|null  $lastName
+     *
+     * @return  self
+     */ 
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    /**
+     * Get pattern="/[0-9]{10}/"
+     *
+     * @return  string|null
+     */ 
+    public function getPhone()
+    {
+        return $this->phone;
+    }
+
+    /**
+     * Set pattern="/[0-9]{10}/"
+     *
+     * @param  string|null  $phone  pattern="/[0-9]{10}/"
+     *
+     * @return  self
+     */ 
+    public function setPhone($phone)
+    {
+        $this->phone = $phone;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of email
+     *
+     * @return  string|null
+     */ 
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set the value of email
+     *
+     * @param  string|null  $email
+     *
+     * @return  self
+     */ 
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of message
+     *
+     * @return  string|null
+     */ 
+    public function getMessage()
+    {
+        return $this->message;
+    }
+
+    /**
+     * Set the value of message
+     *
+     * @param  string|null  $message
+     *
+     * @return  self
+     */ 
+    public function setMessage($message)
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of pokemon
+     *
+     * @return  Pokemon|null
+     */ 
+    public function getPokemon()
+    {
+        return $this->pokemon;
+    }
+
+    /**
+     * Set the value of pokemon
+     *
+     * @param  Pokemon|null  $pokemon
+     *
+     * @return  self
+     */ 
+    public function setPokemon($pokemon)
+    {
+        $this->pokemon = $pokemon;
+
+        return $this;
+    }
+}
